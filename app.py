d27a6ab50c1f78e015c3b4867bd8581885f31b24
unknown <prudhvi.k@mdxblocksinc.com>
2023-10-05 14:09:18 -0500

    working api

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from voice.audio_to_text import audio_check
+from flask_cors import CORS
+import speech_recognition as sr
+from pydub import AudioSegment
+import os
+from io import BytesIO
+import tempfile
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/compare_audio', methods=['GET', 'POST'])
+def audio_to_text():
+    paragraph = request.form.get('paragraph')
+    if 'audio' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    file = request.files['audio']
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+    try:
+        audio = AudioSegment.from_file(file, format="webm", codec="opus")
+        audio = audio.set_channels(1).set_frame_rate(16000)
+    except Exception as e:
+        return jsonify({"error": f"Could not decode file: {e}"}), 400
+
+    return audio_check(audio, paragraph)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ voice/audio_to_text.py
@@ -0,0 +1,75 @@
+import speech_recognition as sr
+import Levenshtein
+from flask import jsonify
+
+
+def text_compare(original_text, audio_text):
+    # Calculate the Levenshtein distance
+    # distance = Levenshtein.distance(original_text, audio_text)
+
+    # # Set a threshold for similarity (adjust as needed)
+    # threshold = 5
+
+    # # Provide feedback based on the distance
+    # if distance == 0:
+    #     feedback = "Perfect! Your pronunciation matches the original text exactly."
+    # elif distance <= threshold:
+    #     feedback = "Good job! Your pronunciation is very close to the original text."
+    # else:
+    #     feedback = "You might want to work on your pronunciation. There are significant differences from the original text."
+
+    # # Print the feedback and Levenshtein distance
+    # print("Feedback:", feedback)
+    # print("Levenshtein Distance:", distance)
+
+    # Calculate the Levenshtein distance
+    distance = Levenshtein.distance(original_text.lower(), audio_text.lower())  # Convert to lowercase for case-insensitive comparison
+
+    # Set a threshold for similarity (adjust as needed)
+    threshold = 5
+
+    # Provide feedback based on the distance
+    if distance == 0:
+        feedback = "Excellent! Your pronunciation matches the original text exactly."
+    elif distance <= threshold:
+        feedback = "Good job! Your pronunciation is very close to the original text."
+    else:
+        feedback = "Your pronunciation needs improvement. Here are some suggestions:\n"
+
+        # Split the original and audio text into words
+        original_words = original_text.split()
+        audio_words = audio_text.split()
+
+        # Compare each word in the text
+        for i in range(min(len(original_words), len(audio_words))):
+            original_word = original_words[i]
+            audio_word = audio_words[i]
+
+            word_distance = Levenshtein.distance(original_word.lower(), audio_word.lower())
+
+            if word_distance > 0:
+                feedback += f"- Replace '{audio_word}' with '{original_word}' in this context.\n"
+
+        # Provide overall feedback based on Levenshtein distance
+        if distance <= threshold + 2:
+            feedback += "Overall, your pronunciation is close to the original text."
+        else:
+            feedback += "Overall, your pronunciation differs significantly from the original text."
+
+    return {"feedback": feedback, "distance": distance}
+
+
+
+def audio_check(audio, original_text):
+    recognizer = sr.Recognizer()
+    audio_data = sr.AudioData(audio.raw_data, audio.frame_rate, audio.frame_width)
+
+    try:
+        audio_text = recognizer.recognize_google(audio_data=audio_data)
+        result = text_compare(original_text, audio_text)
+        print("Comparison Done")
+        return jsonify(result), 200
+    except sr.UnknownValueError:
+        return jsonify({"error": "Could not understand audio"}), 400
+    except sr.RequestError as e:
+        return jsonify({"error": f"Could not request results; {e}"}), 500
